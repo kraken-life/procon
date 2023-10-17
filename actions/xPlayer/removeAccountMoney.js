27c3e66735915fc3b72dbbb23675d3b20763bcef
Tom Witkowski <dev.gummibeer@gmail.com>
2023-10-17 14:31:48 +0200

    move account money check to remove instead of transfer

--- actions/xPlayer/removeAccountMoney.js
+++ actions/xPlayer/removeAccountMoney.js
@@ -1,8 +1,18 @@
 const fetchEsxPlayer = require('../../utils/fetchEsxPlayer');
+const getAccount = require("./getAccount");
 
 module.exports = (data) => {
     const {source, account, amount} = data;
 
-    return fetchEsxPlayer(source)
-        .then((xPlayer) => xPlayer.removeAccountMoney(account, amount));
+    return getAccount({source, account})
+        .then((account) => {
+            if (account.money >= amount) {
+                return fetchEsxPlayer(source)
+                    .then((xPlayer) => xPlayer.removeAccountMoney(account, amount));
+            } else {
+                return false;
+            }
+        });
+
+
 }
--- actions/xPlayer/transferAccountMoney.js
+++ actions/xPlayer/transferAccountMoney.js
@@ -1,21 +1,14 @@
-const getAccount = require('./getAccount');
 const addAccountMoney = require('./addAccountMoney');
 const removeAccountMoney = require('./removeAccountMoney');
 
 module.exports = (data) => {
     const {from, to, account, amount} = data;
 
-    return getAccount({source: from, account})
-        .then((account) => {
-            if (account.money >= amount) {
-                return removeAccountMoney({source: from, account, amount})
-                    .then(() => {
-                        return addAccountMoney({source: to, account, amount})
-                            .then(() => true);
-                    });
-            } else {
-                return false;
-            }
+    return removeAccountMoney({source: from, account, amount})
+        .then((success) => {
+            return success
+                ? addAccountMoney({source: to, account, amount})
+                : false;
         });
 }
 
